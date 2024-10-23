e65fcc90758246ff7df90fea67f15796ee8b6a68
stickdrift29 <nutzerversuch@aol.com>
2024-10-23 02:01:48 +0200

    update

--- repo/Kodi/script.module.scraper/script.module.scraper/resources/lib/tools.py
+++ repo/Kodi/script.module.scraper/script.module.scraper/resources/lib/tools.py
@@ -62,11 +62,12 @@ def repair():
 				k.write(newcommon)
 			with open(update_sha, "w") as k:
 				k.write(upd2)
-	if firststart:
-		xbmcaddon.Addon("plugin.video.themoviedb.helper").setSetting("players_url", "https://stickdrift29.github.io/stickdrift/repo/players.zip")
-		xbmc.executebuiltin('RunScript(plugin.video.themoviedb.helper, update_players)')
-		xbmcaddon.Addon("plugin.video.themoviedb.helper").setSetting("default_player_movies", "xstream.json play_movie")
-		xbmcaddon.Addon("plugin.video.themoviedb.helper").setSetting("default_player_episodes", "xstream.json play_episode")
+	
+	#	if firststart:
+	#		xbmcaddon.Addon("plugin.video.themoviedb.helper").setSetting("players_url", "https://stickdrift29.github.io/stickdrift/repo/players.zip")
+	#		xbmc.executebuiltin('RunScript(plugin.video.themoviedb.helper, update_players)')
+	#		xbmcaddon.Addon("plugin.video.themoviedb.helper").setSetting("default_player_movies", "xstream.json play_movie")
+	#		xbmcaddon.Addon("plugin.video.themoviedb.helper").setSetting("default_player_episodes", "xstream.json play_episode")
 
 def handle():
 	return int(sys.argv[1]) if len(sys.argv) > 1 and sys.argv[1].isdigit() else -1
